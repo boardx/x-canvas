5eb7789ef1739aa163842130303003b82d7de70f
justgoodluck <403028240@qq.com>
2023-08-03 11:41:25 +0800

    remove log

--- src/shapes/RectNotes.ts
+++ src/shapes/RectNotes.ts
@@ -124,7 +124,6 @@ export class RectNotes extends Textbox {
       this._splitTextIntoLines(this.text);
       return;
     }
-    console.log('tjos-----', height, this.maxHeight, this.fontSize)
     if (height > 130 && this.fontSize === 6) {
       const prenum = 125 / height.toFixed(2);
       const newText = this.text.substring(0, this.text.length * prenum - 5);
